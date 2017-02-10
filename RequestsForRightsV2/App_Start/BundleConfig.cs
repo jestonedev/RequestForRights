e6aa986e14a84c2befea969ac410eff55b666872
IgnVV <ignvv@bratsk-city.ru>
2017-02-10 14:36:52 +0800

    Шаманства с расчетом смещения в помощи с учетом недогруженности изображений

--- RequestsForRightsV2/App_Start/BundleConfig.cs
+++ RequestsForRightsV2/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace RequestsForRights
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js").Include(
+                        "~/Scripts/jquery.mousewheel.*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval")
             .Include("~/Scripts/jquery.validate.*")
--- RequestsForRightsV2/Scripts/rr-help.js
+++ RequestsForRightsV2/Scripts/rr-help.js
@@ -3,12 +3,37 @@
     $("body").attr("data-target", "#rr-contents");
 
     setInterval(updateScrollSpy, 1000);
+    var updateLocationHrefTimeout = setTimeout(updateLocationHref, 1000);
+    var notUpdateLocationHref = false;
+
+    $("img")
+        .on("load",
+            function() {
+                updateScrollSpy();
+                updateLocationHref();
+            });
+
+    $("body")
+        .on("click mousewheel keypress",
+            function () {
+                clearTimeout(updateLocationHrefTimeout);
+                notUpdateLocationHref = true;
+                $("img").off("load");
+                $("body").off("click mousewheel keypress");
+            });
 
     function updateScrollSpy() {
         $('[data-spy="scroll"]')
             .each(function() {
                 $(this).scrollspy("refresh");
             });
+    }
+
+    function updateLocationHref() {
+        if (notUpdateLocationHref) return;
+        if ($.inArray("#", location.href) !== -1) {
+            location.href = location.href;
+        }
     }
 
     $(".rr-content p[id]")
